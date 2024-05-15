044e04a33c48fc9c185b952ed43513d916d7b541
iranjitha <kranjitha.sk.29@gmail.com>
2024-05-14 22:14:40 -0700

    new test

---
+++ child/src/App.js
@@ -0,0 +1,32 @@
+
+import React, { useState } from "react";
+import user from "./randomUser.json"
+
+const StateTutorial = () => {
+  
+// const [inputValue, setInputValue] = useState("Uttara");
+//  let onChange = (event) => {
+//   const newValue = event.target.value;
+//   setInputValue(newValue);
+//   console.log(inputValue)
+//   console.log(user)
+// };
+  
+  let inputValue="Uttara";
+
+   let onChange = (event) => {
+    inputValue = event.target.value;
+     console.log(inputValue)
+   };
+
+ 
+
+  return (
+    <div className="flex flex-col items-center justify-center m-10">
+      <input placeholder="enter something..." onChange={onChange} />
+      <div className="text-2xl text-blue-600 mt-10">{inputValue}</div>
+    </div>
+  );
+};
+
+export default StateTutorial;
